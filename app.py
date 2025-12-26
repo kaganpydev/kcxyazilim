32ef2e32b6eb7824e27ef837921f5ea7d8cd7650
KCX Software <kagan.py.dev@gmail.com>
2025-12-26 17:01:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template, Response, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect
+import os
 
 app = Flask(__name__)
+
+
+@app.before_request
+def force_https():
+    if request.headers.get("X-Forwarded-Proto") == "http":
+        return redirect(request.url.replace("http://", "https://"), code=301)
 
 
 @app.route('/')
@@ -19,8 +26,6 @@ def akademi():
 def hakkimizda():
     return render_template("hakkimizda.html")
 
-
-
 @app.route('/akademi/cocuk-python')
 def cocuk_python():
     return render_template("cocuk-python.html")
@@ -29,20 +34,27 @@ def cocuk_python():
 def yetiskin_python():
     return render_template("yetiskin-python.html")
 
+
 @app.route('/sitemap.xml')
 def sitemap():
     return send_from_directory(app.root_path, 'sitemap.xml', mimetype='application/xml')
+
 @app.route('/robots.txt')
 def robots_txt():
     return send_from_directory(app.root_path, 'robots.txt', mimetype='text/plain')
+
+
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(app.static_folder, 'images/favicon.png', mimetype='image/png')
-
-
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/vnd.microsoft.icon'
+    )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
+
 
 
 
